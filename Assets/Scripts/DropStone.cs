dbdacb44078da53f72db13a2b93260dace1c12f6
jeff-maxwell <jeff@tcnsolutions.com>
2019-01-31 20:28:44 -0600

    Broke out Jaws to 2 pieces and altered scripts

---
+++ Assets/Scripts/DropStone.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class DropStone : MonoBehaviour
+{
+    // Stone Prefab transform object
+    public Transform stone;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        // If the key press is a SPACE drop a rock
+        if (Input.GetKeyDown("space"))
+        {
+            // Initialize the rock transform to the game object transform
+            Transform rockSpacing = this.transform;
+
+            // Move the transform location of the rock
+//            rockSpacing.position.Set(rb.transform.position.x,
+//                    rb.transform.position.y - 1,
+//                    rb.transform.position.z);
+
+            stone.GetComponent<SpriteRenderer>().sortingLayerName = "Foreground";
+            stone.GetComponent<SpriteRenderer>().sortingOrder = 5;
+
+            // Create a new instance of the Stone/Rock object
+            Instantiate(stone, this.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/JawsMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JawsMove : MonoBehaviour
+{
+    // Rigid2D tied to the Claw
+    private Rigidbody2D rb;
+
+    // Speed for the movement of the claw left and right
+    public float speed = 0.0f;
+    
+    void Start()
+    {
+        // Get a reference to the Rigid body associated with the script
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        // Get movement value in the Horizontal direction
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        // Create a vector object with the Horizontal move
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+
+        // Add force to the rigid body with them move times the speed
+        rb.AddForce(movement * speed);
+    }
+}

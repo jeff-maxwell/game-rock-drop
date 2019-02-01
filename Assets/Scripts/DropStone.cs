c94991d3ea0ea913428a921ee446942bfed86fb2
jeff-maxwell <jeff@tcnsolutions.com>
2019-01-31 21:48:02 -0600

    Open and Close Claw

--- Assets/Scripts/DropStone.cs
+++ Assets/Scripts/DropStone.cs
@@ -4,30 +4,44 @@ public class DropStone : MonoBehaviour
 {
     // Stone Prefab transform object
     public Transform stone;
+
+    // Closed Claw Sprite
+    public Sprite clawClosed;
+
+    // Open Claw Sprite
+    public Sprite clawOpen;
+
+    private SpriteRenderer spriteRenderer;
     
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
     
     void Update()
     {
-        // If the key press is a SPACE drop a rock
+        // If the key press is a SPACE open the Claw drop a rock
         if (Input.GetKeyDown("space"))
         {
             // Initialize the rock transform to the game object transform
             Transform rockSpacing = this.transform;
 
-            // Move the transform location of the rock
-//            rockSpacing.position.Set(rb.transform.position.x,
-//                    rb.transform.position.y - 1,
-//                    rb.transform.position.z);
+            // Show the Open Claw
+            spriteRenderer.sprite = clawOpen;
 
+            // Set the Sorting Layer for the rock
             stone.GetComponent<SpriteRenderer>().sortingLayerName = "Foreground";
             stone.GetComponent<SpriteRenderer>().sortingOrder = 5;
 
             // Create a new instance of the Stone/Rock object
             Instantiate(stone, this.transform);
+        }
+
+        // If the key released is SPACE close the Claw
+        if (Input.GetKeyUp("space"))
+        {
+            // Show the Closed Claw
+            spriteRenderer.sprite = clawClosed;
         }
     }
 }
